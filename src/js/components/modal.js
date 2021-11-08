fa7ce81190be8c602dc968fb10316c4b35196d4f
Viktor Sinelnykov <Fenya.ua@gmail.com>
2021-11-08 22:26:28 +0200

    add functions close modal window

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -1,10 +1,30 @@
 modal = document.querySelector('[data-modal]');
 openModalBtn = document.querySelector('[data-modal-open]');
 closeModalBtn = document.querySelector('[data-modal-close]');
+backdrop = document.querySelector('.backdrop');
 
-openModalBtn.addEventListener('click', toggleModal);
-closeModalBtn.addEventListener('click', toggleModal);
+openModalBtn.addEventListener('click', openModalWindow);
+closeModalBtn.addEventListener('click', closeModalWindow);
+backdrop.addEventListener('click', closeToBackdrop);
 
-function toggleModal() {
-  modal.classList.toggle('is-hidden');
+function openModalWindow(e) {
+  modal.classList.remove('visually-hidden');
+  window.addEventListener('keydown', onEscKeyPress);
+}
+
+function closeToBackdrop(e) {
+  if (e.target.className === 'backdrop') {
+    closeModalWindow();
+  }
+}
+
+function closeModalWindow() {
+  modal.classList.add('visually-hidden');
+  window.removeEventListener('keydown', onEscKeyPress);
+}
+
+function onEscKeyPress(e) {
+  if (e.code === 'Escape') {
+    closeModalWindow();
+  }
 }
