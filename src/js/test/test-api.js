23b7e89ad147eff588c762a701fcea084c733790
Dron010 <73987873+Dron010@users.noreply.github.com>
2021-11-09 14:44:04 +0200

    update

--- src/js/test/test-api.js
+++ src/js/test/test-api.js
@@ -1,22 +1,32 @@
-// ========= Test API ===============
-import onFetchAllMovies from '../layout/fetch-week-movies';
-import onFetchById from '../layout/fetch-by-Id';
-import onFetchByKeyword from '../layout/fetch-by-keyword';
+// ========= Test API response===============
+import {getApiData} from '../api/api-service.js';
 
-// ----------------------------------
-const callbackTest = res => {
-  console.log(res);
-  console.log(res.results);
+// --------- func for search by ID -------------
+function onFetchById(id) {
+  let query = `/movie/${id}?`;
+
+  return getApiData(query).then(result => result);
+};
+// --------- func for search by keyword (title) -------------
+ function onFetchByKeyword(keyword, page) {
+  let query = `/search/movie/?query=${keyword}&page=${page}`;
+
+  return getApiData(query).then(result => result);
+};
+// --------- func for Main page and for Pagination -------------
+ function onFetchAllMovies(page) {
+  let query = `/trending/movie/week?page=${page}`;
+
+  return getApiData(query).then(result => result);
 };
 
 // ---------------- EXAMPLES ------------------
-
 let page = 1;
 let movieId = 568620;
 let keyword = 'кин-дза-дза';
 
-// onFetchAllMovies(page).then(callbackTest);
+//  onFetchAllMovies(page).then(result=>console.log(result));
 
-// onFetchById(movieId).then(callbackTest);
+//  onFetchById(movieId).then(result=>console.log(result));
 
-// onFetchByKeyword(keyword, page).then(callbackTest);
+//  onFetchByKeyword(keyword, page).then(result=>console.log(result));
