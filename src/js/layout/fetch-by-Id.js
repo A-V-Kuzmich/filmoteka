fee60787b8a62eb40d9a8874738166fcb737f06a
vladislav-slobodenjuk <vladjun@gmail.com>
2021-11-09 03:01:48 +0200

    fetch by id

--- src/js/layout/fetch-by-Id.js
+++ src/js/layout/fetch-by-Id.js
@@ -1,11 +1,39 @@
 import { getApiData } from '../api/api-service.js';
+import makeModalFilm from '../../partial/templates/modal-film.hbs';
+// console.log(makeModalFilm);
 
 // --------- func for search by ID -------------
-function onFetchById(id) {
-    let query = `/movie/${id}?`;
-    getApiData(query)
-      .then(result => {
-        console.log(result);
-      });
+function fetchById(id) {
+  let query = `/movie/${id}?`;
+  return getApiData(query);
+}
+// console.log(fetchById(560));
+// fetchById(560).then(r => console.log(r));
+
+const filmList = document.querySelector('.films');
+// console.log(filmList);
+
+const modal = document.querySelector('.container--main-geometry'); // должна быть модалка
+// console.log(modal);
+
+filmList.addEventListener('click', onCardClick);
+
+function onCardClick(e) {
+  console.log(e);
+  if (e.target.nodeName !== 'UL') {
+    //доблжно быть 'LI'
+    console.log('exit');
+
+    return;
   }
-//  onFetchById(560);
+  console.log('not exit');
+  let filmId = e.target.dataset.index; // id атрибут должен быть на li
+  console.log(filmId);
+
+  fetchById(filmId).then(res => {
+    console.log(res);
+
+    let modalContent = makeModalFilm(res);
+    modal.innerHTML = modalContent;
+  });
+}
