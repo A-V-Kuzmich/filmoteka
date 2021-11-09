052905f3353c7faf66a84344aaf95d7484a4b33f
Bronislav Voydylo <marine4bro@gmail.com>
2021-11-09 21:17:08 +0200

    adds search by keyword

--- src/js/layout/fetch-by-keyword.js
+++ src/js/layout/fetch-by-keyword.js
@@ -1,8 +1,44 @@
 import { getApiData } from '../api/api-service.js';
+import refs from '../refs/refs'
+import filmsTemplate from '../../partial/templates/film-cards.hbs'
 
-// --------- func for search by keyword (title) -------------
-export default function onFetchByKeyword(keyword, page) {
-  let query = `/search/movie/?query=${keyword}&page=${page}`;
+refs.searchFormEl.addEventListener('submit', onSearch)
 
-  return getApiData(query).then(result => result);
+function onFetchByKeyword(keyword, page) {
+    let query = `/search/movie/?query=${keyword}&page=${page}`;
+    
+    return getApiData(query).then
+        (result => {
+               return result;
+            },
+        )
 }
+
+function onSearch(e) {
+    e.preventDefault();
+     
+    clearFilmsMarkup() 
+    const searchQuery = e.currentTarget.elements.query.value
+
+    if (searchQuery === '') {
+        alert('Веддите что-нибудь в строку поиска')
+        return
+    } onFetchByKeyword(searchQuery)
+        .then(result => {
+            if (result.results.length === 0) {
+                alert ('Ничего не найдено, введите что-то людское =)')
+            } createFilmsList(result.results)
+        })
+        .catch(error => alert(error))
+        
+}
+
+function clearFilmsMarkup () {
+    refs.galleryEl.innerHTML = ''
+}
+
+function createFilmsList(films) {
+    refs.galleryEl.insertAdjacentHTML('beforeend', filmsTemplate(films))
+}
+
+
