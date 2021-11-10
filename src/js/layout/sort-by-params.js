11787f177cc2e9fb63a11068d15d98ed53352761
Viktor Kostiuchenko <kostiuchenkoviktor@gmail.com>
2021-11-11 01:35:11 +0300

    +

--- src/js/layout/sort-by-params.js
+++ src/js/layout/sort-by-params.js
@@ -3,6 +3,7 @@ import { createImagesMarkup } from './render-by-template'
 import { refs } from '../refs/refs'
 import { getApiData } from '../api/api-service'
 import { getGenresFromLocalStorage } from './genre-local-storage'
+import { renderImagesToMain } from './render-images-to-main'
 
 function getGenreIdByName(name) {
   const queryGenre = getGenresFromLocalStorage().find(genre => {
@@ -10,25 +11,33 @@ function getGenreIdByName(name) {
   })
   return queryGenre.id
 }
-// getGenreIdByName('Crime')
 
+// function getGenreNameById(genreIds) {
+//   let newArray = []
 
-
-function getGenreNameById(id) {
-  const queryGenre = getGenresFromLocalStorage().find(genre => genre.id === id)
-  return queryGenre.name
-}
-
-// getGenreNameById(12)
+//   genreIds.forEach(genreId => {
+//     const array = getGenresFromLocalStorage().map(genre =>{
+//       if (genre.id === genreId) {
+//         newArray.push(genre.name)
+//       }
+//     })
+//   })
+//   return newArray
+// }
 
 function sortByGenre(value) {
   const genreId = getGenreIdByName(value)
   let query = `/discover/movie?with_genres=${genreId}`;
-  getApiData(query)
-    .then(result => {
-      refs.filmsEl.innerHTML = ''
-      createImagesMarkup(refs.filmsEl, imagesTpl, result.results)
-    });
+  renderImagesToMain(query)
+  // getApiData(query)
+  //   .then(result => {
+  //     result.results.forEach((obj) => {
+  //       obj.genre_ids = getGenreNameById(obj.genre_ids)
+  //       obj.release_date = obj.release_date.slice(0,4)
+  //     })
+  //     refs.filmsEl.innerHTML = ''
+  //     createImagesMarkup(refs.filmsEl, imagesTpl, result.results)
+  //   });
 }
   
 function sortByParameter(parameter) {
