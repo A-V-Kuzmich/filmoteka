5e6725313a1b1b2b5042b168066c7d9fea067b1e
Viktor <Fenya.ua@gmail.com>
2021-11-10 20:15:46 +0200

    test-modal

--- src/js/test/test-modal.js
+++ src/js/test/test-modal.js
@@ -1,37 +1,37 @@
-import { getApiData } from '../api/api-service.js';
-import modalTmpl from "../../partial/templates/modal-film.hbs"
-import { openModalWindow } from "../components/modal.js"
+// import { getApiData } from '../api/api-service.js';
+// import modalTmpl from "../../partial/templates/modal-film.hbs"
+// import { openModalWindow } from "../components/modal.js"
 
-let movieId = 56890;
+// let movieId = 56896;
 
 // const testField = document.querySelector('body');
-const headerField = document.querySelector('.header');
-headerField.insertAdjacentHTML('afterbegin', '<button class="btn-test">!!!!!!!BUTTON ON MODAL Size Chose !!!!!!</button>');
+// const headerField = document.querySelector('.header');
+// headerField.insertAdjacentHTML('afterbegin', '<button class="btn-test">!!!!!!!BUTTON ON MODAL Size Chose !!!!!!</button>');
  
-const btnRef = document.querySelector('.btn-test');
-btnRef.addEventListener('click', () => {
+// const btnRef = document.querySelector('.btn-test');
+// btnRef.addEventListener('click', () => {
   
-    onFetchById(movieId).then(renderCard);
-});
+//     onFetchById(movieId).then(renderCard);
+// });
 
-function onFetchById(id) {
-    let query = `/movie/${id}?`;
+// function onFetchById(id) {
+//     let query = `/movie/${id}?`;
 
-    return getApiData(query).then(result => result);
-};
+//     return getApiData(query).then(result => result);
+// };
 
-function renderCard(card) {
-    let markup = modalTmpl(card)
+// function renderCard(card) {
+//     let markup = modalTmpl(card)
     
-    headerField.insertAdjacentHTML("afterbegin", markup);
-    openModalWindow();
-}
+//     testField.insertAdjacentHTML("afterbegin", markup);
+//     openModalWindow();
+// }
 
-export function removeRender() {
-    const modal = document.querySelector(".modal")
-    modal.innerHTML=""
+// export function removeRender() {
+//     const modal = document.querySelector(".modal")
+//     modal.innerHTML=""
     
-}
+// }
 
 
 
