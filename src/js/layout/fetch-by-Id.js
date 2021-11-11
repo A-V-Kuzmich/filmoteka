5dbd8508c4214f55a307d223e7adbdd6035da059
vladislav-slobodenjuk <vladjun@gmail.com>
2021-11-11 21:31:01 +0200

    stage

--- src/js/layout/fetch-by-Id.js
+++ src/js/layout/fetch-by-Id.js
@@ -2,18 +2,14 @@ import { getApiData } from '../api/api-service.js';
 import makeModalFilm from '../../partial/templates/modal-film.hbs';
 
 import openModalWindow from '../components/modal';
-// console.log(makeModalFilm);
 
 // --------- func for search by ID -------------
 function fetchById(id) {
   let query = `/movie/${id}?`;
   return getApiData(query);
 }
-// console.log(fetchById(560));
-// fetchById(560).then(r => console.log(r));
 
 const filmList = document.querySelector('.films');
-// console.log(filmList);
 
 // const modal = document.querySelector('.modal'); // должна быть модалка
 // console.log(modal);
@@ -23,26 +19,22 @@ filmList.addEventListener('click', onCardClick);
 function onCardClick(e) {
   console.log(e);
   if (e.target.nodeName !== 'IMG') {
-    //доблжно быть 'LI'
     console.log('exit');
-
     return;
   }
   console.log('not exit');
 
-  // openModalWindow();
+  let filmId = e.target.dataset.index; // id атрибут должен быть на елементе клика
+  console.log('film id', filmId);
 
-  let filmId = e.target.dataset.index; // id атрибут должен быть на li
-  console.log(filmId);
+  fetchById(filmId).then(result => {
+    console.log('film object', result);
 
-  fetchById(filmId).then(res => {
-    console.log(res);
-
-    let modalContent = makeModalFilm(res);
-
-    const modal = document.querySelector('body'); // должна быть модалка
+    let modalContent = makeModalFilm(result);
+    const targetEl = document.querySelector('body'); // должна быть модалка
 
     // modal.innerHTML = modalContent;
-    modal.insertAdjacentHTML('afterbegin', modalContent);
+    targetEl.insertAdjacentHTML('afterbegin', modalContent);
+    openModalWindow();
   });
 }
