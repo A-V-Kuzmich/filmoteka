2131b8eff4a3b7caa5d819a6729c3d6e914c1566
vladislav-slobodenjuk <vladjun@gmail.com>
2021-11-11 21:00:58 +0200

    added createInnerMarkup() and crleanInnerMarkup()

--- src/js/layout/render-by-template.js
+++ src/js/layout/render-by-template.js
@@ -1,3 +1,11 @@
 export function createImagesMarkup(element, template, data) {
-  element.insertAdjacentHTML('beforeend', template(data))
+  element.insertAdjacentHTML('beforeend', template(data));
+}
+
+export function createInnerMarkup(element, data) {
+  element.innerHTML = data;
+}
+
+export function crleanInnerMarkup(element) {
+  element.innerHTML = '';
 }
