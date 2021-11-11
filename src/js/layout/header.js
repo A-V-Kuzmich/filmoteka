12390030b12aa3085ef3b4f43052d3d95220a54d
Darya Pashkovskaya <pashkovskayadasha9@gmail.com>
2021-11-11 11:26:00 +0200

    add js for buttons

--- src/js/layout/header.js
+++ src/js/layout/header.js
@@ -1,13 +1,24 @@
 import { refs } from '../refs/refs'
+
 refs.headerNav.addEventListener('click', changeHeader)
+refs.headerBtn.addEventListener('click', changeActiveHeaderBtn)
 
 function changeHeader(e) {
     if (e.srcElement.dataset.action === 'library') {
         toggleClass(refs.header, 'header__main-bckg', 'header__secondary-bckg');
-        inputFormIsHidden()
-    } else if (e.srcElement.dataset.action === 'home') {
-        toggleClass(refs.header, 'header__secondary-bckg', 'header__main-bckg',);
+        inputFormIsHidden();
+        queueBtnActive()
+    } else {
+        toggleClass(refs.header, 'header__secondary-bckg', 'header__main-bckg');
         inputFormVisibility()
+    }
+}
+
+function changeActiveHeaderBtn(e) {
+    if (e.srcElement.dataset.action === 'watched') {
+        watchedBtnActive()
+    } else {
+        queueBtnActive()
     }
 }
 
@@ -20,15 +31,22 @@ function inputFormIsHidden() {
     refs.searchFormEl.classList.add('visually-hidden')
     refs.headerBtn.classList.remove('visually-hidden')
     refs.libraryBtn.classList.add('header__nav-item--active')
-    refs.homeBtn.classList.remove('header__nav-item--active')
-    
+    refs.homeBtn.classList.remove('header__nav-item--active') 
 }
 
 function inputFormVisibility() {
     refs.searchFormEl.classList.remove('visually-hidden')
     refs.headerBtn.classList.add('visually-hidden')
     refs.libraryBtn.classList.remove('header__nav-item--active')
-    refs.homeBtn.classList.add('header__nav-item--active')
-    
+    refs.homeBtn.classList.add('header__nav-item--active') 
 }
 
+function watchedBtnActive() {
+    refs.queueBtn.classList.remove('header___item-btn--active')
+    refs.watchedBtn.classList.add('header___item-btn--active');
+}
+
+function queueBtnActive() {
+    refs.watchedBtn.classList.remove('header___item-btn--active');
+    refs.queueBtn.classList.add('header___item-btn--active');
+}
--- src/js/refs/refs.js
+++ src/js/refs/refs.js
@@ -8,6 +8,8 @@ export const refs = {
     libraryBtn: document.querySelector('[data-action="library"]'),
     headerBtn: document.querySelector('.header__btn'),
     header: document.querySelector('.header'),
-    headerNav:document.querySelector('.header__nav')
+    headerNav: document.querySelector('.header__nav'),
+    watchedBtn: document.querySelector('[data-action="watched"]'),
+    queueBtn: document.querySelector('[data-action="queue"]')
     
   };
