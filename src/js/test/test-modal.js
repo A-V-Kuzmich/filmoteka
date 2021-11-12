6f2c7df64a6d10b497b715dabdff9aa1859e0636
Viktor <Fenya.ua@gmail.com>
2021-11-12 23:34:53 +0200

    close button in backdrop

--- src/js/test/test-modal.js
+++ src/js/test/test-modal.js
@@ -3,14 +3,14 @@ import modalTmpl from '../../partial/templates/modal-film.hbs';
 import { openModalWindow } from '../components/modal.js';
 import { createInnerMarkup } from '../layout/render-by-template.js';
 
-let movieId = 56899;
+// let movieId = 56899;
 
-const testField = document.querySelector('.modal');
-const headerField = document.querySelector('.header');
-headerField.insertAdjacentHTML(
-  'afterbegin',
-  '<button class="btn-test">!!!!!!!BUTTON ON MODAL Size Chose !!!!!!</button>',
-);
+// const testField = document.querySelector('.modal');
+// const headerField = document.querySelector('.header');
+// headerField.insertAdjacentHTML(
+//   'afterbegin',
+//   '<button class="btn-test">!!!!!!!BUTTON ON MODAL Size Chose !!!!!!</button>',
+// );
 const backdropTest = document.querySelector('.backdrop');
 const btnRef = document.querySelector('.btn-test');
 btnRef.addEventListener('click', () => {
