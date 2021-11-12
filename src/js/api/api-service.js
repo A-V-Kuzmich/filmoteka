df4baf431ac5ab7d6ff306e474b860b58dea60dc
Bohdan-Ta <81333397+Bohdan-Ta@users.noreply.github.com>
2021-11-12 13:05:44 +0200

    коррекция функции для остановки spiner

--- src/js/api/api-service.js
+++ src/js/api/api-service.js
@@ -6,10 +6,9 @@ axios.defaults.baseURL = 'https://api.themoviedb.org/3';
 const API_KEY = 'a3ec7c1621ade0b1491e66cd43b88745';
 export const getApiData = query => {
 showSpiner();
-  return axios
-    .get(`${query}&api_key=${API_KEY}`)
-    .then(response => {
-      hideSpiner() 
-      return response.data})
-    .catch(onFetchError)
+return axios
+.get(`${query}&api_key=${API_KEY}`)
+.then(response => response.data)
+.catch(onFetchError)
+.finally(hideSpiner())
 };
