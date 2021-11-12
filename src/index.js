fd1f0921e2ec03ee4dd67c4a5ed0379d3269414a
LarysaTymoshenko <luda76@ukr.net>
2021-11-12 18:17:31 +0200

    1817

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './sass/main.scss';
 
 //=========layout=================
 import './js/refs/refs.js';
-
+import './js/layout/switchTheme.js';
 //functions
 import './js/layout/fetch-by-Id';
 import './js/layout/render-by-params';
@@ -21,7 +21,7 @@ import './js/layout/header';
 import './js/components/backdrop.js';
 import './js/components/modal.js';
 import './js/components/notifications.js';
-import './js/components/switchTheme.js';
+import './js/layout/switchTheme.js';
 
 //=============api==================
 import './js/api/api-service.js';
