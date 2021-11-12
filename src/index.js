611e437ab0c630fcf115fd5789774eddcc95d9cc
Serhii Sokolov <sokolovserega1982@gmail.com>
2021-11-12 21:16:50 +0200

    del link on test click-on-logo

--- src/index.js
+++ src/index.js
@@ -27,6 +27,5 @@ import './js/api/api-service.js';
 
 //=============test==================
 import './js/test/test-api.js';
-// import './js/test/test-click-on-logo.js';
 import './js/test/test-modal.js';
 // import './js/test/test-notification.js';
