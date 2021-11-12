198a373d480d50fcc489804b85edc07edc34862c
vladislav-slobodenjuk <vladjun@gmail.com>
2021-11-12 02:29:05 +0200

    add to queue

--- src/js/layout/add-to-queue.js
+++ src/js/layout/add-to-queue.js
@@ -1,5 +1,39 @@
 // const addBtn = document.querySelector('[data-add]');
+import { setToLocalStorage } from './local-storage';
+import { getFromLocalStorage } from './local-storage';
 
 export function addToQueue(e) {
-  console.log(e);
+  console.dir(e.target.dataset.hasOwnProperty('queue'));
+
+  if (!e.target.dataset.hasOwnProperty('queue')) {
+    console.log('not add-button', false);
+    return;
+  }
+  console.log('it is add-button', true);
+
+  const id = e.target.dataset.id;
+  console.log('id', id);
+  // console.log(localStorage.getItem(key));
+
+  if (!localStorage.getItem('queue')) {
+    console.log('no queue at localStorage');
+    const queue = {
+      list: [],
+    };
+    queue.list.push(id);
+    console.log('queueArray', queue);
+    setToLocalStorage('queue', queue);
+  } else {
+    const storageQueue = getFromLocalStorage('queue');
+    console.log('item from local', storageQueue);
+
+    console.log(storageQueue);
+    const newStorageQueue = storageQueue.list.push(id);
+    // console.log(newStorageQueue);
+
+    setToLocalStorage('queue', newStorageQueue);
+  }
 }
+
+// console.log(localStorage);
+// setToLocalStorage();
--- src/js/layout/fetch-by-Id.js
+++ src/js/layout/fetch-by-Id.js
@@ -1,60 +1,48 @@
 import { getApiData } from '../api/api-service.js';
-import makeModalFilm from '../../partial/templates/modal-film.hbs';
+// import { createImagesMarkup } from './render-by-template';
 
-// import addToQueue from './add-to-queue.js';
-// console.log(addToQueue);
-import openModalWindow from '../components/modal';
-// console.log(makeModalFilm);
+import makeModalFilm from '../../partial/templates/modal-film.hbs';
+import { openModalWindow } from '../components/modal.js';
+
+import { addToQueue } from './add-to-queue';
 
 // --------- func for search by ID -------------
 function fetchById(id) {
   let query = `/movie/${id}?`;
   return getApiData(query);
 }
-// console.log(fetchById(560));
-// fetchById(560).then(r => console.log(r));
 
 const filmList = document.querySelector('.films');
-// console.log(filmList);
-
 // const modal = document.querySelector('.modal'); // должна быть модалка
-// console.log(modal);
 
 filmList.addEventListener('click', onCardClick);
 
 function onCardClick(e) {
   console.log(e);
   if (e.target.nodeName !== 'IMG') {
-    //доблжно быть 'LI'
-    console.log('exit');
-
+    // console.log('exit');
     return;
   }
-  console.log('not exit');
+  // console.log('not exit');
 
-  let filmId = e.target.dataset.index; // id атрибут должен быть на li
-  console.log(filmId);
+  let filmId = e.target.dataset.index; // id атрибут должен быть на елементе клика
+  console.log('film id', filmId);
 
-  fetchById(filmId).then(res => {
-    console.log(res);
+  fetchById(filmId).then(result => {
+    // console.log('film object', result);
 
-    let modalContent = makeModalFilm(res);
+    const modalContent = makeModalFilm(result);
+    const targetEl = document.querySelector('body'); // должна быть модалка
 
-    const modal = document.querySelector('body'); // должна быть модалка
+    // modal.innerHTML = modalContent; // должна быть замена содержимого модалки при каждом открытии
+    targetEl.insertAdjacentHTML('afterbegin', modalContent);
 
-    // modal.innerHTML = modalContent;
-    modal.insertAdjacentHTML('afterbegin', modalContent);
+    // createImagesMarkup(targetEl, makeModalFilm, result);
     openModalWindow();
 
-    const addBtn = document.querySelector('[data-add]');
-    console.log(addBtn);
+    const addBtn = document.querySelector('[data-queue]');
+    // console.log(addBtn);
 
     addBtn.addEventListener('click', addToQueue);
   });
 }
-
-// function addToQueue(e) {
-//   console.log(e);
-
-//   localStorage.setItem(key, JSON.stringify(data));
-// }
