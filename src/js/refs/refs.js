443b4ea337f54bcd72273fc044cb65c3af1dc379
Dron010 <73987873+Dron010@users.noreply.github.com>
2021-11-13 19:11:55 +0200

    +

--- src/js/refs/refs.js
+++ src/js/refs/refs.js
@@ -1,7 +1,7 @@
 export const refs = {
-    
+    body: document.querySelector('body'),
     modal: document.querySelector('[data-modal]'),
     filmsEl: document.querySelector('.films'),
     searchFormEl: document.querySelector('.header__form'),
     inputBtnEl: document.querySelector('.js-input-btn'),
-  };
+  }
--- src/js/test/test-video-player.js
+++ src/js/test/test-video-player.js
@@ -1,4 +1,6 @@
-import {getApiData} from '../api/api-service.js';
+import { getApiData } from '../api/api-service.js';
+// import { body } from '../refs/refs.js';
+ const body = document.querySelector('body');
 
 function onFetchById(id) {
     const type = 'images,videos&language=ru-RU'
@@ -14,15 +16,23 @@ onFetchById(movieId).then(result => console.log(result));
   
 //    N1r36HTysDM
 //const player = document.querySelector('#player')
-var player;
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player('player', {
-    height: '360',
-    width: '480',
-    videoId: 'M7lc1UVf-VE',
-    events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
-    }
-  });
+// var player;
+// function onYouTubeIframeAPIReady() {
+//   player = new YT.Player('player', {
+//     height: '360',
+//     width: '480',
+//     videoId: 'M7lc1UVf-VE',
+//     events: {
+//       'onReady': onPlayerReady,
+//       'onStateChange': onPlayerStateChange
+//     }
+//   });
+// }
+
+
+function openVideoPlayer() {
+  body.classList.add('.no-scroll');
 }
+// openVideoPlayer();
+
+  // body.classList.add('no-scroll');
