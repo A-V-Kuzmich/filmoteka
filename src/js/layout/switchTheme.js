86d509cd240ada92c463bfa8ff355f1652f83e5a
LarysaTymoshenko <luda76@ukr.net>
2021-11-14 14:08:27 +0200

    1401

--- src/js/layout/switchTheme.js
+++ src/js/layout/switchTheme.js
@@ -8,17 +8,17 @@ const theme = {
 
 const { LIGHT, DARK } = theme;
 
-let newTheme = getFromLocalStorage('n-theme');
+// let newTheme = getFromLocalStorage('n-theme');
 
-if (!newTheme) {
-  newTheme = LIGHT;
-  setToLocalStorage('n-theme', LIGHT);
-} else document.querySelector('body').classList.add(newTheme);
-// refs.clickBox.click = newTheme === LIGHT ? false : true;
+// if (!newTheme) {
+//   newTheme = LIGHT;
+//   setToLocalStorage('n-theme', LIGHT);
+// } else document.querySelector('body').classList.add(newTheme);
+// // refs.clickBox.click = newTheme === LIGHT ? false : true;
 
-// refs.clickBox.addEventListener('click', function () {
-//   document.body.classList.toggle(DARK);
-// });
+// // refs.clickBox.addEventListener('click', function () {
+// //   document.body.classList.toggle(DARK);
+// // });
 
 refs.clickBox.addEventListener('click', changeTheme);
 
