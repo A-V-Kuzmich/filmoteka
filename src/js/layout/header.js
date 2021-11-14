06f34aeea5689f2912adb3d8907c3a9be0dadc5f
Darya Pashkovskaya <pashkovskayadasha9@gmail.com>
2021-11-14 17:25:52 +0200

    fixed header buttons

--- src/js/layout/header.js
+++ src/js/layout/header.js
@@ -4,20 +4,21 @@ refs.headerNav.addEventListener('click', changeHeader);
 refs.headerBtn.addEventListener('click', changeActiveHeaderBtn);
 
 function changeHeader(e) {
-  if (e.srcElement.dataset.action === 'library' || e.srcElement.dataset.action === 'home') {
-    refs.header.classList.toggle('header__main-bckg');
-    refs.header.classList.toggle('header__secondary-bckg');
+  if (e.srcElement.dataset.action === 'library' || 'home') {
     changeInputCondition();
+    libraryBtnActive();
   } 
 }
 
 function changeActiveHeaderBtn(e) {
-  if (e.srcElement.dataset.action === 'watched' || e.srcElement.dataset.action === 'queue') {
+  if (e.srcElement.dataset.action === 'watched' ||'queue') {
     changeActiveBtn();
   } 
 }
 
 function changeInputCondition() {
+  refs.header.classList.toggle('header__main-bckg');
+  refs.header.classList.toggle('header__secondary-bckg');
   refs.headerSearcherEl.classList.toggle('visually-hidden')
   refs.headerBtn.classList.toggle('visually-hidden')
   refs.libraryBtn.classList.toggle('header__nav-item--active')
@@ -27,4 +28,9 @@ function changeInputCondition() {
 function changeActiveBtn() {
   refs.queueBtn.classList.toggle('header__item-btn--active');
   refs.watchedBtn.classList.toggle('header__item-btn--active');
+}
+
+function libraryBtnActive() {
+  refs.watchedBtn.classList.remove('header__item-btn--active');
+  refs.queueBtn.classList.add('header__item-btn--active');
 }
