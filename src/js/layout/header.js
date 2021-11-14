5cafef0bdd535ec588a619d4af6b010761851b2d
Darya Pashkovskaya <pashkovskayadasha9@gmail.com>
2021-11-14 02:09:27 +0200

    fixed header js

--- src/js/layout/header.js
+++ src/js/layout/header.js
@@ -4,47 +4,27 @@ refs.headerNav.addEventListener('click', changeHeader);
 refs.headerBtn.addEventListener('click', changeActiveHeaderBtn);
 
 function changeHeader(e) {
-  if (e.srcElement.dataset.action === 'library') {
-    doLibraryView();
-  } else if (e.srcElement.dataset.action === 'home') {
-    doMainView();
-  }
+  if (e.srcElement.dataset.action === 'library' || e.srcElement.dataset.action === 'home') {
+    refs.header.classList.toggle('header__main-bckg');
+    refs.header.classList.toggle('header__secondary-bckg');
+    changeInputCondition();
+  } 
 }
 
 function changeActiveHeaderBtn(e) {
-  if (e.srcElement.dataset.action === 'watched') {
-    watchedBtnActive();
-  } else if (e.srcElement.dataset.action === 'queue') {
-    queueBtnActive();
-  }
+  if (e.srcElement.dataset.action === 'watched' || e.srcElement.dataset.action === 'queue') {
+    changeActiveBtn();
+  } 
 }
 
-function doMainView() {
-  toggleClass(refs.header, 'header__secondary-bckg', 'header__main-bckg');
-  inputFormToggleClass();
-  watchedBtnActive();
+function changeInputCondition() {
+  refs.headerSearcherEl.classList.toggle('visually-hidden')
+  refs.headerBtn.classList.toggle('visually-hidden')
+  refs.libraryBtn.classList.toggle('header__nav-item--active')
+  refs.homeBtn.classList.toggle('header__nav-item--active') 
 }
-
-function inputFormIsHidden() {
-    refs.headerSearcherEl.classList.add('visually-hidden')
-    refs.headerBtn.classList.remove('visually-hidden')
-    refs.libraryBtn.classList.add('header__nav-item--active')
-    refs.homeBtn.classList.remove('header__nav-item--active') 
-}
-
-function inputFormVisibility() {
-    refs.headerSearcherEl.classList.remove('visually-hidden')
-    refs.headerBtn.classList.add('visually-hidden')
-    refs.libraryBtn.classList.remove('header__nav-item--active')
-    refs.homeBtn.classList.add('header__nav-item--active') 
-}
-
-function watchedBtnActive() {
+ 
+function changeActiveBtn() {
   refs.queueBtn.classList.toggle('header__item-btn--active');
   refs.watchedBtn.classList.toggle('header__item-btn--active');
-}
-
-function queueBtnActive() {
-  refs.watchedBtn.classList.remove('header__item-btn--active');
-  refs.queueBtn.classList.add('header__item-btn--active');
 }
