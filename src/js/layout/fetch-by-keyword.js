755119dcc609272e27f12e53dedcb985523467f6
Bronislav Voydylo <marine4bro@gmail.com>
2021-11-14 15:22:58 +0200

    fixed pagination

--- src/js/layout/fetch-by-keyword.js
+++ src/js/layout/fetch-by-keyword.js
@@ -1,45 +1,80 @@
 import { getApiData } from '../api/api-service.js';
 import { refs } from '../refs/refs'
 import filmsTemplate from '../../partial/templates/film-cards.hbs'
-import { createInnerMarkup, cleanInnerMarkup } from './render-by-template'
+import { createInnerMarkup, cleanInnerMarkup} from './render-by-template'
 import { alertEnterQuery } from '../components/notifications'
 import { alertNothingIsFound } from '../components/notifications'
-import { exchangeObjectData } from './render-images-to-main'
-
-refs.searchFormEl.addEventListener('submit', onSearch)
-let searchQuery = ''
+import { getGenresFromLocalStorage } from './genre-local-storage'
+let searchQuery = '1'
 let onClickPage = 1;
 let totalPages = 0;
 let btnSummary = 2
-
-function onFetchAllMovies(page) {
-    let query = `/trending/movie/week?page=${page}`;
-    cleanInnerMarkup(refs.filmsEl)
+onFetchAllMovies('/trending/movie/week?page=1')
+refs.paginationList.addEventListener('click', onPaginationBtnClick)
+refs.filtersDropdownEl.addEventListener('click', searhByParameter)
+refs.searchFormEl.addEventListener('submit', onSearch)
+function searhByParameter(evt) {
+  const yearItem = evt.target.classList.contains('values__form-input')
+  const genreItem = evt.target.classList.contains('values__item--genre')
+  const ratingItem = evt.target.classList.contains('values__item--rating')
+  switch (true) {
+    case genreItem:
+      searchByGenre(evt.target.textContent);
+      break
+    case yearItem:
+      searchByRelease();
+      break
+    case ratingItem:
+      searchByPopularity(evt.target.textContent)
+      break
+  }
+}
+function searchByGenre(value) {
     cleanInnerMarkup(refs.paginationBtnList)
-    return getApiData(query)
-        .then(result => {
-            exchangeObjectData(result)
-            setLastPageNumber(result.total_pages)
-            createInnerMarkup(refs.filmsEl, filmsTemplate(result.results))
-            renderPagesList(result.total_pages)
-            currentBtnClass()
-      })
+    const genreId = getGenreIdByName(value)
+    onClickPage = 1
+    searchQuery = ''
+    searchQuery = `/discover/movie?with_genres=${genreId}&sort_by=popularity.desc`;
+    renderImages(searchQuery, refs.filmsEl, filmsTemplate)
 }
-
-function renderGall(page) {
-    onFetchAllMovies(page)
+function searchByYear(e) {
+  cleanInnerMarkup(refs.paginationBtnList)
+  e.preventDefault();
+    const year = e.currentTarget.elements.year.value
+  onClickPage = 1
+  searchQuery = ''
+  searchQuery = `/discover/movie?primary_release_year=${year}&sort_by=popularity.desc`
+  renderImages(searchQuery, refs.filmsEl, filmsTemplate)
 }
-
-renderGall(1)
-
+function searchByRelease() {
+    cleanInnerMarkup(refs.paginationBtnList)
+    onClickPage = 1
+    let thisYear = new Date().getFullYear();
+    refs.valuesInput.setAttribute("max", thisYear);
+    refs.valueFormEl.addEventListener('submit', searchByYear)
+}
+function searchByPopularity(value) {
+    cleanInnerMarkup(refs.paginationBtnList)
+    onClickPage = 1
+    searchQuery = ''
+    searchQuery = `/discover/movie?sort_by=popularity.${value}`;  
+    if (value === 'descending') {
+        value = value.slice(0,4)
+    } else {
+        value = value.slice(0,3)
+        }
+    renderImages(searchQuery, refs.filmsEl, filmsTemplate)
+}
 function onFetchByKeyword(keyword, page) {
-    let query = `/search/movie/?query=${keyword}&page=${page}`;
-    
-    return getApiData(query).then
+    cleanInnerMarkup(refs.paginationBtnList)
+    searchQuery = ''
+    searchQuery = `/search/movie/?query=${keyword}&page=${page}`;
+    return getApiData(searchQuery).then
         (result => {
             if (result.results.length === 0) {
                 alertNothingIsFound()
             }
+            console.log(result);
             exchangeObjectData(result)
             setLastPageNumber(result.total_pages)
             createInnerMarkup(refs.filmsEl, filmsTemplate(result.results))
@@ -47,52 +82,74 @@ function onFetchByKeyword(keyword, page) {
             currentBtnClass()
         })
 }
-
-function onSearch(e) {
-    e.preventDefault();
-    searchQuery = e.currentTarget.elements.query.value
-    onClickPage = 1
-    cleanInnerMarkup(refs.paginationBtnList)
-    if (searchQuery === '') {
-        alertEnterQuery()
-        return
-    } 
-    onFetchByKeyword(searchQuery)
-}
-
-
-// !!!! pagination !!!
-
-function setLastPageNumber(totalPages) {
-    refs.lastPaginationBtn.textContent = totalPages
-}
-
-refs.paginationList.addEventListener('click', onPaginationBtnClick)
-
-
-function renderPagesList(totalPages) {
-    const start = onClickPage - btnSummary
-    const end = onClickPage + btnSummary;
-    
-    for (let i = start; i <= end; i += 1) {
-    if (i > 1 && i < totalPages) {
-      refs.paginationBtnList.insertAdjacentHTML('beforeend', `<li class="pagination__list-item"><button type="button" class="pagination__list-item">${i}</button></li>`,
-      );}
-  }
-}
-
 function onPaginationBtnClick(evt) {
     cleanInnerMarkup(refs.paginationBtnList)
     if (evt.target.nodeName !== 'BUTTON') {
         return;
-    } onClickPage = Number(evt.target.textContent);
-    if (searchQuery === '') {
-        onFetchAllMovies(onClickPage)
-        return
-    } else { onFetchByKeyword(searchQuery, onClickPage)
-        }
+    }
+    onClickPage = Number(evt.target.textContent);
+    const newPage = `${searchQuery}&page=${onClickPage}`
+    renderImages(newPage, refs.filmsEl, filmsTemplate)
 }
-
+function getGenreIdByName(name) {
+  const queryGenre = getGenresFromLocalStorage().find(genre => {
+    return genre.name === name[0].toUpperCase() + name.slice(1).toLowerCase()
+  })
+  return queryGenre.id
+}
+function getGenreNameById(genreIds) {
+  let newArray = []
+  genreIds.forEach(genreId => {
+    getGenresFromLocalStorage().map(genre =>{
+      if (genre.id === genreId) {
+        newArray.push(genre.name)
+      }
+    })
+  })
+  return newArray
+}
+function exchangeObjectData(result) {
+  result.results.forEach((obj) => {
+    if (obj.genre_ids) {
+    obj.genre_ids = getGenreNameById(obj.genre_ids)}
+    if (obj.release_date) {
+    obj.release_date = obj.release_date.slice(0, 4)
+    }
+  })
+}
+function renderImages(query, element, template) {
+  getApiData(query)
+      .then(result => {
+        console.log(result);
+      exchangeObjectData(result);
+      setLastPageNumber(result.total_pages)
+      createInnerMarkup(element, template(result.results))
+      renderPagesList(result.total_pages)
+      currentBtnClass()
+    }
+  );
+}
+function onFetchAllMovies(page) {
+    cleanInnerMarkup(refs.filmsEl)
+    cleanInnerMarkup(refs.paginationBtnList)
+    searchQuery = ''
+    searchQuery = `/trending/movie/week?`;
+    renderImages(searchQuery, refs.filmsEl, filmsTemplate)
+}
+function onSearch(e) {
+    e.preventDefault();
+    const keyword = e.currentTarget.elements.query.value
+    onClickPage = 1
+    cleanInnerMarkup(refs.paginationBtnList)
+    if (keyword === '') {
+        alertEnterQuery()
+        return
+    }
+    onFetchByKeyword(keyword)
+}
+function setLastPageNumber(totalPages) {
+    refs.lastPaginationBtn.textContent = totalPages
+}
 function currentBtnClass() {
   let paginationBtns = refs.paginationList.querySelectorAll('button');
   for (let i = 0; i < paginationBtns.length; i += 1) {
@@ -103,4 +160,12 @@ function currentBtnClass() {
     }
   }
 }
-
+function renderPagesList(totalPages) {
+    const start = onClickPage - btnSummary
+    const end = onClickPage + btnSummary;
+    for (let i = start; i <= end; i += 1) {
+    if (i > 1 && i < totalPages) {
+      refs.paginationBtnList.insertAdjacentHTML('beforeend', `<li class="pagination__list-item"><button type="button" class="pagination__list-item">${i}</button></li>`,
+      );}
+  }
+}
