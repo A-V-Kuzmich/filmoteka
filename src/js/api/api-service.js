b44d1cf59f2fc863ba286ba1e25eba9781a373ea
Bohdan-Ta <81333397+Bohdan-Ta@users.noreply.github.com>
2021-11-16 01:06:18 +0200

    commit

--- src/js/api/api-service.js
+++ src/js/api/api-service.js
@@ -1,13 +1,25 @@
 import axios from 'axios';
 import { onFetchError } from '../components/notifications.js';
 import { hideSpiner, showSpiner } from '../components/spiner.js';
+// import  { select }  from '../refs/refs'
+
+const select = document.querySelector('select');
+select.addEventListener('change', changeLanguages);
+
+function changeLanguages () {
+    const leng = select.value;
+    console.log(leng)
+
+};
+    
+const LG = 'uk';
 
 axios.defaults.baseURL = 'https://api.themoviedb.org/3';
 const API_KEY = 'a3ec7c1621ade0b1491e66cd43b88745';
 export const getApiData = query => {
 showSpiner();
 return axios
-.get(`${query}&api_key=${API_KEY}`)
+.get(`${query}&api_key=${API_KEY}&language=${LG}`)
 .then(response => {
     hideSpiner()
     return response.data
---
+++ src/js/components/translate.js
@@ -0,0 +1,20 @@
+const langArrey = {
+    "name": {
+        "ua": "фільмотека",
+        "ru": "фильмотека",
+        "en": "filmoteka",
+    },
+
+    "home": {
+        "ua": "до дому",
+        "ru": "домой",
+        "en": "home",
+    },
+    
+    "my-libruary": {
+        "ua": "моя бібліотека",
+        "ru": "моя библиотека",
+        "en": "my libruary",
+    },    
+
+}
