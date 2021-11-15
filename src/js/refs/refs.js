fa97c35137f7eb644054fa8515252a9701b98a8b
Bohdan-Ta <81333397+Bohdan-Ta@users.noreply.github.com>
2021-11-15 23:51:33 +0200

    смена языка через select

--- src/js/refs/refs.js
+++ src/js/refs/refs.js
@@ -23,4 +23,5 @@ export const refs = {
   firstPaginationBtn: document.querySelector('#first-btn'),
   lastPaginationBtn: document.querySelector('#last-btn'),
   clickBox: document.querySelector('#theme-switch-toggle'),
+  // select: document.querySelector('select'),//клас для переключения языка
 };
