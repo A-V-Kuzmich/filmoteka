190e4e0487ca943f39c17c094b718bd7d17ce768
Bohdan-Ta <81333397+Bohdan-Ta@users.noreply.github.com>
2021-11-16 17:00:24 +0200

    дописал функцию перевода

--- src/js/layout/changeLanguages.js
+++ src/js/layout/changeLanguages.js
@@ -1,9 +1,8 @@
-// import { langArrey } from "../components/translate";
 const langArrey = {
     "header-logo": {
-        "uk": "фільмотека",
-        "ru": "фильмотека",
-        "en": "filmoteka",
+        "uk": "Фільмотека",
+        "ru": "Фильмотека",
+        "en": "Filmoteka",
     },
 
     "home": {
@@ -17,6 +16,33 @@ const langArrey = {
         "ru": "моя библиотека",
         "en": "my libruary",
     },    
+
+    "watched": {
+        "uk": "ПЕРЕГЛЯНУТІ",
+        "ru": "ПРОСМОТРЕННЫЕ",
+        "en": "WATCHED",
+    },  
+    "queue": {
+        "uk": "НА ЧЕРЗІ",
+        "ru": "В ОЧЕРЕДИ",
+        "en": "QUEUE",
+    }, 
+    // "vote": {
+    //     "uk": "Рейтинг/Голоси",
+    //     "ru": "Рейтинг/Голоса",
+    //     "en": "Vote/Votes",
+    // }, 
+    // "add-watched": {
+    //     "uk": "Додати переглянуті",
+    //     "ru": "Дополнить просмотренные",
+    //     "en": "Add-watched",
+    // },
+    "right": {
+        "uk": "&#169; 2020 | Всі Права Захищені | Зроблено Студентами GоIT",
+        "ru": "&#169; 2020 | Все Права Защищены | Сделано Студентами GоIT",
+        "en": "&#169; 2020 | All Rights Reserved | Developed by GoIT Student's",
+    }
+
 
 };
 
