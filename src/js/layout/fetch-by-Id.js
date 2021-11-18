d2d6507b99da2224d6b7b033cc9f8d2db8df298c
Dron010 <73987873+Dron010@users.noreply.github.com>
2021-11-18 22:56:11 +0200

    Update fetch-by-Id.js

---
+++ src/js/layout/fetch-by-Id.js
@@ -0,0 +1,59 @@
+import { getApiData } from '../api/api-service.js';
+import { createInnerMarkup } from './render-by-template';
+
+import makeModalFilm from '../../partial/templates/modal-film.hbs';
+import { openModalWindow } from '../components/modal.js';
+
+import { refs } from '../refs/refs.js';
+import { addToStorageArray } from './add-to-storage-array';
+import { openVideo } from '../components/video-player';
+
+// --------- func for search by ID -------------
+export function fetchById(id) {
+  let query = `/movie/${id}?`;
+  return getApiData(query);
+}
+
+const { filmsEl, modal, backdrop } = refs;
+
+filmsEl.addEventListener('click', onCardClick);
+
+//---------click tracking function----------------
+function onCardClick(e) {
+  switch (e.srcElement.className) {
+    case 'film__trailer':
+      openVideo(getId(e));
+      break;
+    case 'film__trailer-img':
+      openVideo(getId(e));
+      break;
+    case 'films':
+      break;
+    default:
+      openModalCard(getId(e));
+  }
+}
+//-----------getting an ID card------------------------
+function getId(e) {
+  return e.path.find(num => num.className === 'films__item').dataset.id;
+}
+//---------opening a modal window---------------------
+function openModalCard(filmId) {
+  fetchById(filmId).then(result => {
+    const id = filmId;
+    // popularity = result.popularity.toFixed(2); - ломало открытие модалки временно закомментировал
+    const modalContent = makeModalFilm(result);
+
+    createInnerMarkup(modal, modalContent);
+    openModalWindow(backdrop);
+
+    const openVideoBtn = document.querySelector('[data-modal="modal-video-btn"]');
+    openVideoBtn.addEventListener('click', () => openVideo(id));
+
+    const addToQueueBtn = document.querySelector('[data-queue]');
+    addToQueueBtn.addEventListener('click', addToStorageArray('queue', 'queue'));
+
+    const addToWatchedBtn = document.querySelector('[data-watched]');
+    addToWatchedBtn.addEventListener('click', addToStorageArray('watched', 'watched'));
+  });
+}
