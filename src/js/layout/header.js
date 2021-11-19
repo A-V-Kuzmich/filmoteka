656c7046c99872ac6698d937b078a93a574e0cbb
vladislav-slobodenjuk <vladjun@gmail.com>
2021-11-19 17:20:04 +0200

    updt

--- src/js/layout/header.js
+++ src/js/layout/header.js
@@ -1,10 +1,19 @@
-
 import { refs } from '../refs/refs';
 import { renderFromStorageArray } from './render-storage-array';
 import { onFetchAllMovies } from './week-movies';
 
-
-const { headerNav, headerBtn, libraryBtn, queueBtn, watchedBtn, header, headerSearcherEl, homeBtn, paginationList } = refs;
+const {
+  headerNav,
+  headerBtn,
+  libraryBtn,
+  queueBtn,
+  watchedBtn,
+  header,
+  headerSearcherEl,
+  homeBtn,
+  paginationList,
+  emptyListImg,
+} = refs;
 
 headerNav.addEventListener('click', changeHeader);
 headerBtn.addEventListener('click', changeActiveHeaderBtn);
@@ -13,45 +22,46 @@ libraryBtn.addEventListener('click', renderFromStorageArray('queue'));
 function changeHeader(e) {
   switch (e.srcElement.dataset.action) {
     case 'library':
-      choseActiveEl(queueBtn, watchedBtn, 'header__item-btn--active')
+      choseActiveEl(queueBtn, watchedBtn, 'header__item-btn--active');
       selectLibraryBtn();
-      paginationList.classList.add('visually-hidden')
+      paginationList.classList.add('visually-hidden');
       break;
     case 'home':
       selectHomeBtn();
       onFetchAllMovies(1);
-      paginationList.classList.remove('visually-hidden')
+      paginationList.classList.remove('visually-hidden');
+      emptyListImg.classList.add('visually-hidden');
       break;
   }
 }
 
 export function returnToMain() {
-      selectHomeBtn();
-      onFetchAllMovies(1);
-      paginationList.classList.remove('visually-hidden')
+  selectHomeBtn();
+  onFetchAllMovies(1);
+  paginationList.classList.add('visually-hidden');
 }
 
 function changeActiveHeaderBtn(e) {
   switch (e.srcElement.dataset.action) {
     case 'watched':
-      choseActiveEl(watchedBtn, queueBtn, 'header__item-btn--active')
+      choseActiveEl(watchedBtn, queueBtn, 'header__item-btn--active');
       break;
     case 'queue':
-      choseActiveEl(queueBtn, watchedBtn, 'header__item-btn--active')
+      choseActiveEl(queueBtn, watchedBtn, 'header__item-btn--active');
       break;
   }
 }
 
 function selectLibraryBtn() {
   header.classList.replace('header__main-bckg', 'header__secondary-bckg');
-  choseActiveEl(libraryBtn, homeBtn,'header__nav-item--active')
-  choseActiveEl(headerSearcherEl, headerBtn, 'visually-hidden')
+  choseActiveEl(libraryBtn, homeBtn, 'header__nav-item--active');
+  choseActiveEl(headerSearcherEl, headerBtn, 'visually-hidden');
 }
 
 function selectHomeBtn() {
   header.classList.replace('header__secondary-bckg', 'header__main-bckg');
-  choseActiveEl(headerBtn, headerSearcherEl, 'visually-hidden')
-  choseActiveEl(homeBtn, libraryBtn,'header__nav-item--active')
+  choseActiveEl(headerBtn, headerSearcherEl, 'visually-hidden');
+  choseActiveEl(homeBtn, libraryBtn, 'header__nav-item--active');
 }
 
 function choseActiveEl(activate, deactivate, condition) {
